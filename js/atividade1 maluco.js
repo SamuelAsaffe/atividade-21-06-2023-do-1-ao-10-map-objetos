19c6abb6a5932cd3d5b22ec8c40ea0f1216a2823
SamuelAsaffe <samuelasaffe1@gmail.com>
2023-06-22 16:54:32 -0300

    1 pronta

--- js/atividade1 maluco.js
+++ js/atividade1 maluco.js
@@ -5,14 +5,13 @@ nomes = ['sivir', 'silvio santos', 'shivana', 'samuel', 'samara',
     'mathews', 'marta', 'marlon', 'mario', 'marilia', 'maria', 'maria',
     'marcelo', 'lucas', 'judas', 'joão', 'jonas', 'cirilo']
 let notas = [3.5, 9.3, 10, 4.3, 5.6, 7.9, 10.0, 4.6, 7.5, 6.0, 3.7, 9.8, 6.4, 5.3, 9.2, 6.7, 6.6, 8.3]
-let aluno = {}
 let magico = []
 for (let i = 0; i < nomes.length; i++) {
     // notas.push((Math.random() * 10) + 1.0).toFixed(1)
     aluno = {
-        nome: nomes[i],
-        nota: notas[i],
-        // situacao: notas[i] >= 7.0 ? "Aprovado" : "Reprovado"
+        "nome: ": nomes[i],
+        "nota: ": notas[i],
+        "situacao: ": notas[i] >= 7.0 ? "Aprovado" : "Reprovado"
     }
     magico.push(aluno);
 
